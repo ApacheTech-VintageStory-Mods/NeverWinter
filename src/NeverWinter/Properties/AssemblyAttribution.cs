641636f8f6cd79d19e3ac02968b0e94ec2bd7423
ApacheTech Solutions <p_matthews@live.co.uk>
2025-03-08 23:58:44 +0000

    Synchronising changes across repositories

--- src/NeverWinter/Properties/AssemblyAttribution.cs
+++ src/NeverWinter/Properties/AssemblyAttribution.cs
@@ -7,7 +7,7 @@
     "neverwinter",
     Description = "Allows fine-grained control over temperatures, and seasons.",
     Side = "Universal",
-    Version = "1.6.0",
+    Version = "1.6.2",
     Website = "https://apachetech.co.uk",
     Contributors = ["Rythillian"],
     Authors = ["ApacheTechSolutions"])]
