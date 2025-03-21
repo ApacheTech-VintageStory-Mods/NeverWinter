97a460e0a65dbf459650109929bf856dbbbf1799
ApacheTech Solutions <p_matthews@live.co.uk>
2025-03-21 01:34:15 +0000

    fix: compatibilty after game update

--- src/NeverWinter/Properties/AssemblyAttribution.cs
+++ src/NeverWinter/Properties/AssemblyAttribution.cs
@@ -1,13 +1,13 @@
 ﻿// ReSharper disable StringLiteralTypo
 
-[assembly: ModDependency("game", "1.20.1")]
+[assembly: ModDependency("game", "1.20.6")]
 
 [assembly: ModInfo(
     "NeverWinter",
     "neverwinter",
     Description = "Allows fine-grained control over temperatures, and seasons.",
     Side = "Universal",
-    Version = "1.6.2",
+    Version = "1.6.3",
     Website = "https://apachetech.co.uk",
     Contributors = ["Rythillian"],
     Authors = ["ApacheTechSolutions"])]
